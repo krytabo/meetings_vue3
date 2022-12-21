a2f07f451a635c6c00782de05ad62ce9fbd2a382
Krystal Tabo <krytabo@gmail.com>
2022-12-21 09:26:58 +0800

    first

--- src/views/config/api.js
+++ src/views/config/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const dataList = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://krytabo.github.io/meetings_vue3",
 });
 
-export const userList = () => dataList.get("/memberList");
-export const meetingList = () => dataList.get("/meetingList");
-export const patch_meetingList = (data) => dataList.patch("/meetingList", data);
-export const to_doList = () => dataList.get("/toDoList");
+export const userList = () => dataList.get("/memberList.json");
+export const meetingList = () => dataList.get("/meetingList.json");
+export const patch_meetingList = (data) => dataList.patch("/meetingList.json", data);
+export const to_doList = () => dataList.get("/toDoList.json");
