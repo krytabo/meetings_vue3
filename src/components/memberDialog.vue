63079fc64e1baa3935f19550ba1421aa0289a8da
Krystal Tabo <krytabo@gmail.com>
2022-12-30 17:53:51 +0800

    first

--- src/components/memberDialog.vue
+++ src/components/memberDialog.vue
@@ -1,20 +1,5 @@
 <template>
-  <!--<el-dialog title="選擇人員" draggable>
-    <div class="flax space-x-4">
-      <div class=""></div>
-      <el-table :data="memberList" ref="multipleTable" @selection-change="select">
-        <el-table-column type="selection" width="50"></el-table-column>
-        <el-table-column label="編號" prop="id"></el-table-column>
-        <el-table-column label="姓名" prop="name"></el-table-column>
-        <el-table-column label="部門" prop="department"></el-table-column>
-      </el-table>
-    </div>
-
-    &lt;!&ndash;    <el-input v-model="searchText"></el-input>&ndash;&gt;
-    <el-button @click="confirmButton">確定</el-button>
-  </el-dialog>-->
-
-  <el-dialog title="選擇人員" draggable :visible="dialogFormVisible" :show-close="false">
+  <el-dialog title="選擇人員" draggable v-model="dialogFormVisible" :show-close="false">
     <div class="flax mb-5 space-x-4">
       <el-table ref="memberTables" :data="memberList" @selection-change="select">
         <el-table-column type="selection" width="50"></el-table-column>
@@ -66,38 +51,7 @@ export default {
       this.memberList = res.data;
     });
   },
-  computed: {
-    dialogVisible: {
-      get() {
-        return this.visible;
-      },
-      set(val) {
-        // 当visible改变的时候，触发父组件的 updateVisible方法，在该方法中更改传入子组件的 centerDialogVisible的值
-        this.$emit("updateVisible", val);
-        console.log(123);
-        this.$nextTick(() => {
-          for (let i = 0; i < this.$refs.multipleTable.length; i++) {
-            this.$refs.memberTables[i].clearSelection();
-            this.tags.forEach((item) => {
-              this.memberList.forEach((subItem) => {
-                if (item.id === subItem.id) {
-                  this.$refs.memberTables[i].toggleRowSelection(subItem, true);
-                }
-              });
-            });
-          }
-          /*this.$refs.memberTables.clearSelection();
-          this.tags.forEach((item) => {
-            this.memberList.forEach((subItem) => {
-              if (item.id === subItem.id) {
-                this.$refs.memberTables.toggleRowSelection(subItem, true);
-              }
-            });
-          });*/
-        });
-      },
-    },
-  },
+  computed: {},
   methods: {
     // table選擇框
     select(val) {
@@ -112,7 +66,7 @@ export default {
     },
     // 取消按鈕
     cancelButton() {
-      this.$emit("resetPopupData");
+      this.$emit("resetPopupData", false);
     },
   },
   watch: {
